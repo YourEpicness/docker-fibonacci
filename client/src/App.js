3503b0d879e7197f969fdc0a3b69639e97f73b5b
Julaam Diop <jeebe.diop@gmail.com>
2021-06-09 18:47:11 -0400

    added all project files

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+
+import Fib from './pages/Fib';
+import OtherPage from './pages/OtherPage';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/"> Home </Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
